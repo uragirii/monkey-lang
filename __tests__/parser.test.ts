a9299274ef3a9df8ccced064441057c560c0645d
Apoorv Kansal <apoorvkansalak@gmail.com>
2022-09-17 00:29:53 +0530

    test: added tests for let statements

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -1,5 +1,13 @@
+import { LetStatement } from '../src/ast';
 import { Lexer } from '../src/lexer';
 import { Parser } from '../src/parser';
+import { TOKENS } from '../src/token';
+
+const testLetStatements = (statement: LetStatement, value: string) => {
+  expect(statement.tokenLiteral().toUpperCase()).toBe(TOKENS.LET);
+  expect(statement.name.value).toBe(value);
+  expect(statement.name.tokenLiteral()).toBe(value);
+};
 
 describe('tests parser', () => {
   it('tests with only let statements', () => {
@@ -17,5 +25,11 @@ describe('tests parser', () => {
     expect(program).toBeDefined();
 
     expect(program.statements.length).toBe(3);
+
+    const letStatementValues = ['x', 'y', 'foobar'];
+
+    program.statements.forEach((statement, i) =>
+      testLetStatements(statement as LetStatement, letStatementValues[i]),
+    );
   });
 });
